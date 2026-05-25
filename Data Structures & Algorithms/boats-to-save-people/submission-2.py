ced591de69ff410af30b5147dad64ba0fd7121a5
Aaryan Raj <raj.0123.aaryan.work@gmail.com>
2026-05-26 01:55:26 +0530

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.py
@@ -0,0 +1,85 @@
+# class Solution:
+#     def numRescueBoats(self, people: List[int], limit: int) -> int:
+#         m = max(people)
+#         count = [0] * (m + 1)
+#         for p in people:
+#             count[p] += 1
+
+#         idx, i = 0, 1
+#         while idx < len(people):
+#             while count[i] == 0:
+#                 i += 1
+#             people[idx] = i
+#             count[i] -= 1
+#             idx += 1
+
+#         res, l, r = 0, 0, len(people) - 1
+#         while l <= r:
+#             remain = limit - people[r]
+#             r -= 1
+#             res += 1
+#             if l <= r and remain >= people[l]:
+#                 l += 1
+#         return res
+
+# class Solution:
+#     def numRescueBoats(self, people: List[int], limit: int) -> int:
+#         count = [0 for i in range(limit+1)]
+#         result = 0
+#         for p in people:
+#             count[p] += 1
+        
+
+#         left = 1
+#         right = limit
+
+#         while left < right:
+            
+#             leftCount = count[left]
+#             if leftCount == 0:
+#                 left+=1
+#                 continue
+#             rightCount = count[right]
+#             if rightCount == 0:
+#                 right-=1
+#                 continue
+#             if left + right > limit:
+#                 result+= rightCount
+#                 right-=1
+#             else:
+#                 if leftCount > rightCount:
+#                     result += rightCount
+#                     count[left]-=rightCount
+#                     count[right]-=rightCount
+#                     right-=1
+#                 elif leftCount < rightCount:
+#                     result += leftCount
+#                     count[left]-=leftCount
+#                     count[right]-=leftCount
+#                     left+=1
+#                 else:
+#                     result += leftCount
+#                     count[left]-=leftCount
+#                     count[right]-=leftCount
+#                     left+=1
+#                     right-=1
+        
+#         if count[left]!=0:
+#             peoplePerBoat = min(limit//left,2)
+#             result += math.ceil(count[left]/peoplePerBoat)
+            
+#         return result
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        l,r = 0,len(people) - 1
+        boats = 0
+        while l<=r:
+            if people[l] + people[r] <= limit:
+                l += 1
+            r -= 1
+            boats += 1
+        return boats
+            
+
